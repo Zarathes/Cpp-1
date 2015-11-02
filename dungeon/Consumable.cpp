157b6776fb2672da527a14d4dffdaaae2f382521
schees <schees@avans.nl>
2015-11-02 16:16:07 +0100

    Consumable methods gegeven

--- dungeon/Consumable.cpp
+++ dungeon/Consumable.cpp
@@ -1,1 +1,18 @@
 #include "stdafx.h"
+#include "Consumable.h"
+
+void Consumable::setName(std::string nameNew){
+	name = nameNew;
+}
+
+void Consumable::setDescription(std::string descr){
+	description = descr;
+}
+
+std::string Consumable::getName(){
+	return name;
+}
+
+std::string Consumable::getDescription(){
+	return description;
+}
--- dungeon/Consumable.h
+++ dungeon/Consumable.h
@@ -1,5 +1,13 @@
 #include "Item.h"
 
 class Consumable : public Item{
-
+public:
+	void setName(std::string nameNew);
+	void setDescription(std::string descr);
+	std::string getName();
+	std::string getDescription();
+	void use();
+private:
+	std::string name;
+	std::string description;
 };
